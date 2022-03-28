ff25291bb216a9dd07b9a7248b1ccba5be329e55
Patrik Vaďura <patrikvadura@icloud.com>
2022-03-28 14:25:35 +0200

    add: klub + agent

---
+++ agent.php
@@ -0,0 +1,60 @@
+<?php
+    include 'header.php';
+
+    // hero
+    include 'components/hero/simple/hero-agent.finomsport.php';
+
+    $web = $_GET['web'];
+    $data = json_decode(file_get_contents("data/".$web.".json"));
+    // content
+?>
+    <div class="wrap post2col post2col-01">
+        <?php
+            foreach ($data->pages->agent->posts as $item) { ?>
+                <div class="item mb-2">
+                    <div class="image">
+                        <div class="badge">
+                            <?php echo $item->sport; ?>
+                        </div>
+
+                        <img src="<?php echo $item->img; ?>">
+                    </div>
+
+                    <div class="content">
+                        <h2>
+                            <?php echo $item->title; ?>
+                        </h2>
+
+
+                        <h3>
+                            <?php echo $item->subtitle; ?>
+                        </h3>
+
+                        <p>
+                            <?php echo $item->description; ?>
+                        </p>
+                    </div>
+                </div>
+            <?php }
+        ?>
+    </div>
+
+    <div class="benefits benefits-01">
+        <div class="wrapS">
+            <div class="grid-2">
+                <?php
+                foreach ($data->pages->agent->benefits as $item) { ?>
+                    <div class="item">
+                        <img src="https://res.cloudinary.com/patrik-vadura/image/upload/v1648042795/Finom%20Sport/icon_check_y7apzj.svg">
+                        <?php echo $item->title; ?>
+                    </div>
+                <?php }
+                ?>
+            </div>
+        </div>
+    </div>
+<?php
+    // contact
+    include 'components/contact/cta/contact-cta.finomsport.php';
+
+    include 'footer.php';
