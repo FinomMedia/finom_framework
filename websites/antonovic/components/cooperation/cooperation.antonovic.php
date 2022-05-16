bdbcb263e3df7acd7069672ff087d4e2ea462f49
Patrik Vaďura <patrikvadura@icloud.com>
2022-05-16 13:41:05 +0200

    fix: ?web=antonovic

--- websites/antonovic/components/cooperation/cooperation.antonovic.php
+++ websites/antonovic/components/cooperation/cooperation.antonovic.php
@@ -1,37 +1,29 @@
-<?php
-    $cooperationTitle = "Spolupráce";
-    $profileImage = "https://res.cloudinary.com/patrik-vadura/image/upload/v1645624054/antonovic_web/spoluprace_profile_helu7h.png";
-    $cooperationImage = "https://res.cloudinary.com/patrik-vadura/image/upload/v1645626361/antonovic_web/spoluprace_design-block_01_j4xjxx.png";
-    $cooperationImageMobile = "https://res.cloudinary.com/patrik-vadura/image/upload/v1645627270/antonovic_web/spoluprace_design-block_01_mobile_granu4.png";
-?>
-
-
-<div id="cooperation" class="cooperation cooperation-01">
+<div id="<?=$componentId?>" class="cooperation <?=$classes?>">
     <div class="wrap">
         <div class="grid-2">
             <div>
                 <div class="imageBox">
-                    <img src="<?php echo $profileImage; ?>" alt="cooperation">
+                    <img src="<?=$profileImage?>" alt="cooperation">
 
                     <div class="description">
                         <h2>
-                            <?php echo $cooperationTitle; ?>
+                            <?=$title?>
                         </h2>
 
                         <p>
-                            “Naši spolupráci stavíme na hodnotách, odpovědného přístupu, důvěry a fér jednání. Naši největší hodnotu pak tvoří lidé, kteří tyhle hodnoty představují. Máte stejný postoj, tak vítejte mezi námi.”
+                            <?=$description?>
                         </p>
 
-                        <a href="#" class="btn btnOutlinedPrimary mt-2" target="_blank">
-                            Dozvědět se více
+                        <a href="<?=$buttonLink?>" class="btn btnOutlinedPrimary mt-2" target="_blank">
+                            <?=$button?>
                         </a>
                     </div>
                 </div>
             </div>
 
             <div>
-                <img class="desktopImage" src="<?php echo $cooperationImage; ?>" alt="cooperation">
-                <img class="mobileImage" src="<?php echo $cooperationImageMobile; ?>" alt="cooperation">
+                <img class="desktopImage" src="<?=$image->desktop?>" alt="cooperation">
+                <img class="mobileImage" src="<?=$image->mobile?>" alt="cooperation">
             </div>
         </div>
     </div>
