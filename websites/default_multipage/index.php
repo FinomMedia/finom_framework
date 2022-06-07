909d7f7aede2a3061bf85060c1ef3b07312213cb
Ondrej Hastik <ondrejhastik@gmail.com>
2022-06-07 13:06:46 +0200

    edit: cleaning

--- websites/default_multipage/index.php
+++ websites/default_multipage/index.php
@@ -1,6 +1,5 @@
 <?php
     
-
     $segment = basename(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
     if(!$segment){
@@ -9,11 +8,3 @@
     else{
         include "pages/$segment.php";
     }
-
-    
-    /*include 'partials/header.php';
-
-    component("_blank.component", $data->blank);
-
-    include 'partials/footer.php';*/
-
