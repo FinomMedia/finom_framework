0626e407d6d3bf2ac5c728d877cb90adb15db1f0
Ondrej Hastik <ondrejhastik@gmail.com>
2022-06-15 12:03:08 +0200

    edit: Recursive Matrix render finish

--- templater/xc_matrix_renderer.php
+++ templater/xc_matrix_renderer.php
@@ -2,21 +2,34 @@
 
     include "xc_component.php";
 
+    $indexFrom = 0;
+
 	function repeaterItemsToTree($arr, $currentLevel = 0) {
+        global $indexFrom;
 		$root = array();
 	  
+        $i=0;
 		foreach ($arr as $elem){
-		  if ($elem->depth == $currentLevel) {
-			$root[] = $elem;
-			
-		  } else if ($elem->depth == $currentLevel + 1) {
-			$root[count($root)-1]->subitems = repeaterItemsToTree($arr,$elem->depth);      
-		  }
+         
+          if($i>=$indexFrom){
+            echo "Procházím el. n. $i";
+            if ($elem->depth == $currentLevel) {
+                $root[] = $elem;
+                $indexFrom++;
+                
+              } else if ($elem->depth == $currentLevel + 1) {
+                $root[count($root)-1]->subitems = repeaterItemsToTree($arr,$elem->depth);      
+              }
+              else{
+                  return $root;
+              }
+          }
+		  
+          $i++;
 		}
 	  
 		return $root;
 	  }
-
 
     function componentMatrixRender($matrix_field_name = "xcf_content_matrix"){
 		$matrix_array = wire()->page->$matrix_field_name;
