c6cb3a344b1523f0f4a9d3db2bc1971781ab3c4b
Ondrej Hastik <ondrejhastik@gmail.com>
2022-06-16 07:59:31 +0200

    edit: Page template scafold changes

--- templates/page_layouts/default.php
+++ templates/page_layouts/default.php
@@ -1,10 +1,22 @@
 <?php namespace ProcessWire; ?>
 
-<br><br>
-<div id="content">
-	Toto je obsah layoutu -- title je -- <?=$page->title?>
-</div>	
-<br><br>
+
+
+<?php include __DIR__."/../partials/header.php" ?>
+
+<?php if($cache->get("pager")): ?>
+	<?=$cache->get("pager") ?>
+<?php else: ?>
+
+	<?php $value = Templater::getPartial("content") ?>
+	<?php $cache->save("pager", $value, 1) ?>
+	<?= $value ?>
+
+<?php endif ?>
+<?php bd(Templater::$less->getCss()); ?>
+</div>
+
+<?php include __DIR__."/../partials/footer.php" ?>
 
 
 
